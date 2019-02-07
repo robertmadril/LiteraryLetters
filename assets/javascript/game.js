140e83007a3bd74a8ccc9223c34a98d3f53240a5
robertmadril <robertmmadril@gmail.com>
2019-02-07 13:52:22 -0700

    add pseudocode to js

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+/*
+
+pick a random word from an Array, total win count to 3, total guess count to 10
+
+Display appropriate number of blank lines on screen
+
+if user clicks correct key display letter in correct spot
+    else display letter in 'letters used' and deduct one guess
+
+if user's guesses reach 0, display 'you ran out of guesses and display answer.
+
+when user completes full word display image of ____ and add to win count
+
+create new word when user clicks 'new word'
+*/
