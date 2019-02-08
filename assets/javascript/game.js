35fe78e0fa0ff462c6ff2481fc69b8eec4e404a7
robertmadril <robertmmadril@gmail.com>
2019-02-08 10:03:07 -0700

    add js logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,41 +4,50 @@ vars holding array, random word from an Array, total win count to 3, total guess
 
 */
 
-var words = ["Book", "Book2", "Book3", "Book4"]
+var words = ["r", "Book2", "Book3", "Book4"]
 var winCount = 0;
 var lossCount = 0;
 var guessCount = 10;
 
 var guessWordText = document.getElementById("guess-word");
-var userChoiceText = document.getElementById("userchoice-text");
 var guessCountText = document.getElementById("guess-cnt");
 var winsText = document.getElementById("win-cnt");
 var lossesText = document.getElementById("loss-cnt");
 
 //Display appropriate number of blank lines on screen
-var wordLength = //
 
-var randomWord = words[Math.floor(Math.random() * randomWord.length)];
-
-    document.onkeyup = function (event) {
+//var wordLength = ;
+//var randomWord = "r";
 
 
-        /*if user clicks correct key display letter in correct spot
-        else display letter in 'letters used' and deduct one guess */
+//var randomWord = words[Math.floor(Math.random() * computerChoices.length)];
 
-        if (randomWord.indexOf(userInpt) > -1) {
-            //display text in appropriate spot
-        }
-        else {
-            guesscount = guessCount - 1
-        }
+document.onkeyup = function (event) {
 
+    var userGuess = event.key
+
+    /*if user clicks correct key display letter in correct spot
+    else display letter in 'letters used' and deduct one guess */
+
+    if (words.indexOf(userGuess) > -1) {
+        //display text in appropriate spot
+    }
+    else {
+        guesscount = guessCount - 1
     }
 
-if (guessCount = 0) {
-    //display 'you ran out of guesses and display answer.
 
-    lossCount = lossCount + 1
+    if (guessCount = 0) {
+        //display 'you ran out of guesses and display answer.
+
+        lossCount = lossCount + 1
+    } 
+
+    guessWordText.textContent = "You chose: " + userGuess;
+    computerChoiceText.textContent = "The computer chose: " + computerGuess;
+    winsText.textContent = "Wins: " + wins;
+    lossesText.textContent = "Losses: " + losses;
+    tiesText.textContent = "Ties: " + ties;
 }
 
 /*
