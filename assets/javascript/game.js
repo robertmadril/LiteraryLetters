85dba69af302d7cbd5db88eeac8371055ab7c6dd
robertmadril <robertmmadril@gmail.com>
2019-02-08 09:44:31 -0700

    add js var and logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,48 @@
 /*
 
-pick a random word from an Array, total win count to 3, total guess count to 10
+vars holding array, random word from an Array, total win count to 3, total guess count to 10
 
 */
 
 var words = ["Book", "Book2", "Book3", "Book4"]
-
-var winCount = 3;
-
+var winCount = 0;
+var lossCount = 0;
 var guessCount = 10;
 
-Math.random(words);
+var guessWordText = document.getElementById("guess-word");
+var userChoiceText = document.getElementById("userchoice-text");
+var guessCountText = document.getElementById("guess-cnt");
+var winsText = document.getElementById("win-cnt");
+var lossesText = document.getElementById("loss-cnt");
+
+//Display appropriate number of blank lines on screen
+var wordLength = //
+
+var randomWord = words[Math.floor(Math.random() * randomWord.length)];
+
+    document.onkeyup = function (event) {
+
+
+        /*if user clicks correct key display letter in correct spot
+        else display letter in 'letters used' and deduct one guess */
+
+        if (randomWord.indexOf(userInpt) > -1) {
+            //display text in appropriate spot
+        }
+        else {
+            guesscount = guessCount - 1
+        }
+
+    }
+
+if (guessCount = 0) {
+    //display 'you ran out of guesses and display answer.
+
+    lossCount = lossCount + 1
+}
 
 /*
-
-Display appropriate number of blank lines on screen
-
-if user clicks correct key display letter in correct spot
-    else display letter in 'letters used' and deduct one guess
-
-if user's guesses reach 0, display 'you ran out of guesses and display answer.
+NEED
 
 when user completes full word display image of ____ and add to win count
 
