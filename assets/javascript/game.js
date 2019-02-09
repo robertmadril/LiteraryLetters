5bb8f5dc44e364b5c22ef3baaf7fe46b01a9912a
robertmadril <robertmmadril@gmail.com>
2019-02-09 14:14:47 -0700

    add js structure

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,45 +14,88 @@ var guessCountText = document.getElementById("guess-cnt");
 var winsText = document.getElementById("win-cnt");
 var lossesText = document.getElementById("loss-cnt");
 
-//Display appropriate number of blank lines on screen
+var displayWord = randomWord.length;
 
-var randomWord = words[Math.floor(Math.random() * words.length)];;
+var mystWord = randomWord(words);
+
+function randomWord(array) {
+    return array[Math.floor(Math.random() * array.length)].toUpperCase()
+};
+
+
+
+//split prop to insert into set blank lines
+
+function fillInWordWithLetter(letter, word) {
+    return word.toLowerCase().split('').map(l => {
+        if (l === letter) {
+            return l.toUpperCase()
+        }
+        return '_'
+    }).join(' ')
+};
+
+
 
 
 document.onkeyup = function (event) {
 
-    var userGuess = event.key;
+    if (event.key === "1") {
 
-    console.log(randomWord)
+        console.log(mystWord);} }
 
-    /*if user clicks correct key display letter in correct spot
-    else display letter in 'letters used' and deduct one guess */
+document.onkeyup = function (event) {
 
-    if (randomWord.indexOf(userGuess) > -1) {
+            var userGuess = event.key;
 
-        for (i = 0; i < words.length; i++) {
-            if (randomWord[i] === userGuess) {
-                console.log(words[i]);
+            var mystWordLetter =  () => {
+                for (i=0; i<mystWord.length; i++) {
+                    return mystWord[i];
+                }
+            };
+
+            if (event.key === mystWordLetter) {
+                console.log(event.key);
             }
-        }
-        //display text in appropriate spot
-    }
-    else {
-        guesscount = guessCount - 1
-    }
+
+            fillInWordWithLetter(userGuess, mystWord);
 
 
-    if (guessCount = 0) {
-        //display 'you ran out of guesses and display answer.
 
-        lossCount = lossCount + 1
-    } 
 
-    guessWordText.textContent = "You chose: " + userGuess;
-    guessCountText.textContent = guessCount;
-    winsText.textContent = "Wins: " + winCount;
-    lossesText.textContent = "Losses: " + lossCount;
-}
+            /*if user clicks correct key display letter in correct spot
+            else display letter in 'letters used' and deduct one guess */
+
+            if (mystWord.indexOf(userGuess) > -1) {
+
+                for (i = 0; i < randomWord.length; i++) {
+                    if (randomWord[i] === userGuess) {
+                        console.log(words[i]);
+                    }
+                    else {
+                        guesscount = guessCount - 1
+                    }
+                    //display text in appropriate spot
+                }
+
+                //Display appropriate number of blank lines on screen, or input
+
+            };
+
+            guessWordText.textContent = "You chose: " + userGuess;
+            guessCountText.textContent = guessCount;
+            winsText.textContent = "Wins: " + winCount;
+            lossesText.textContent = "Losses: " + lossCount;
+        };
+        console.log(guessCount);
+
+
+        if (guessCount = 0) {
+            //display 'you ran out of guesses and display answer.
+
+            lossCount = lossCount + 1
+        };
+
 
 /*
 NEED
