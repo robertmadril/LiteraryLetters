32307481d67fa861871b0d434b70595a6ed006f7
robertmadril <robertmmadril@gmail.com>
2019-02-10 12:54:28 -0700

    guess functionality add

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,96 +1,75 @@
-/*
 
-vars holding array, random word from an Array, total win count to 3, total guess count to 10
-
-*/
-
-var words = ["r", "Book2", "Book3", "Book4"]
+//holds array of potential mystery words
+var words = ["booksalot", "book", "sirbookington", "bookabilly"]
+//holds initial win/loss counts
 var winCount = 0;
 var lossCount = 0;
+//holds initial guess count
 var guessCount = 10;
+//empty array to hold mistaken guesses
+var guesses = [];
+//uses randomword() function to pull a mystery word randomly from words array
+var mystWord = randomWord(words);
+//splits mystery word into individual letters
+var displayWord = mystWord.split("");
+
 
 var guessWordText = document.getElementById("guess-word");
 var guessCountText = document.getElementById("guess-cnt");
 var winsText = document.getElementById("win-cnt");
 var lossesText = document.getElementById("loss-cnt");
 
-var displayWord = randomWord.length;
 
-var mystWord = randomWord(words);
 
-function randomWord(array) {
-    return array[Math.floor(Math.random() * array.length)].toUpperCase()
+function randomWord(arr) {
+    return arr[Math.floor(Math.random() * arr.length)].toUpperCase()
 };
 
 
-
-//split prop to insert into set blank lines
-
-function fillInWordWithLetter(letter, word) {
-    return word.toLowerCase().split('').map(l => {
-        if (l === letter) {
-            return l.toUpperCase()
-        }
-        return '_'
-    }).join(' ')
+function blankSpace(ar) {
+    for (i=0; i<ar.length; i++) {
+        return " _ ";
+    }
 };
 
+//test functioning
+console.log(displayWord);
+console.log(blankSpace(mystWord));
+console.log(mystWord);
 
 
-
-document.onkeyup = function (event) {
-
-    if (event.key === "1") {
-
-        console.log(mystWord);} }
 
 document.onkeyup = function (event) {
 
             var userGuess = event.key;
 
-            var mystWordLetter =  () => {
-                for (i=0; i<mystWord.length; i++) {
-                    return mystWord[i];
-                }
+
+            if (mystWord.indexOf(userGuess)>-1) {
+                console.log("Correct guess");
+
             };
 
-            if (event.key === mystWordLetter) {
-                console.log(event.key);
+
+            if (mystWord.indexOf(userGuess)===-1) {
+                console.log("Incorrect guess");
+                guesses.push(userGuess);
+                guessCount--;
             }
 
-            fillInWordWithLetter(userGuess, mystWord);
-
-
-
-
-            /*if user clicks correct key display letter in correct spot
-            else display letter in 'letters used' and deduct one guess */
-
-            if (mystWord.indexOf(userGuess) > -1) {
-
-                for (i = 0; i < randomWord.length; i++) {
-                    if (randomWord[i] === userGuess) {
-                        console.log(words[i]);
-                    }
-                    else {
-                        guesscount = guessCount - 1
-                    }
-                    //display text in appropriate spot
-                }
+            //test
+            console.log(guesses);
+            console.log(guessCount);
 
                 //Display appropriate number of blank lines on screen, or input
 
-            };
-
-            guessWordText.textContent = "You chose: " + userGuess;
-            guessCountText.textContent = guessCount;
+            //guessWordText.textContent = "You chose: " + userGuess;
+            guessCountText.textContent = "You have " + guessCount + " guesses remaining.";
             winsText.textContent = "Wins: " + winCount;
             lossesText.textContent = "Losses: " + lossCount;
         };
-        console.log(guessCount);
 
 
-        if (guessCount = 0) {
+        if (guessCount === 0) {
             //display 'you ran out of guesses and display answer.
 
             lossCount = lossCount + 1
