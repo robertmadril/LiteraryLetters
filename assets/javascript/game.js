631fccf8008c64a86b28f9131a0ad833c3dada25
robertmadril <robertmmadril@gmail.com>
2019-02-12 11:57:42 -0700

    add image, book cover functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,9 @@
 
 //holds array of potential mystery words
 
-var words = ["Don Quixote", "Emma", "Call of the Wild", "The Great Gatsby", "Nineteen Eighty Four", "Lord of the Rings", "On the Road", ];
+var words = ["donquixote", "emma", "callofthewild", "thegreatgatsby", "nineteeneightyfour", "lordoftherings", "ontheroad"];
+//image array
+var images = ["assets/images/donquixote.jpg", "assets/images/emma.jpg", "assets/images/callofthewild.jpg", "assets/images/greatgatsby.jpg", "assets/images/1984.jpg", "assets/images/lotr.jpg", "assets/images/ontheroad.jpg"]
 //holds initial win/loss counts
 var winCount = 0;
 var lossCount = 0;
@@ -21,7 +23,9 @@ var lossesText = document.getElementById("loss-cnt");
 var startPage = document.getElementById("start");
 var solved = document.getElementById("answer");
 var guessString = document.getElementById("guess-strng");
-
+var openBook = document.getElementById("open-book");
+var closedBook = document.getElementById("closed-book");
+var changeImage = document.getElementById("change-img");
 
 //function starts new game while keeping win/loss count
 function newGame() {
@@ -53,8 +57,8 @@ function checkWord(l) {
 function checkWin() {
     if (randomWord === blankArr.join("")) {
         //display 'you ran out of guesses and display answer.
-        alert("You Won!");
         winCount = winCount + 1;
+        bookCover();
         //starts new game, keeps win/loss
         newGame();
 
@@ -64,18 +68,48 @@ function checkWin() {
 function checkLoss() {
     //checks if guess count is at zero
     if (guessCount < 1) {
-        //display 'you ran out of guesses and display answer.
-        alert("You lost :(")
+        //display 'you ran out of guesses and display cover.
         lossCount = lossCount + 1;
         //starts new game, keeps win/loss
+        bookCover();
         newGame();
     };
+}
+
+function keepScore() {
+        //displays array without commas with .join method
+        guessWordText.textContent = blankArr.join("");
+        winsText.textContent = "Wins: " + winCount;
+        lossesText.textContent = "Losses: " + lossCount;
+        startPage.textContent = ""
+        guessString.textContent = guesses;
+        guessCountText.textContent = "You have " + guessCount + " guesses remaining.";
+}
+
+function indBookCover() {
+    for (i = 0; i<words.length;i++) {
+        if (words[i] === randomWord) {
+            changeImage.src = images[i];
+        }
+    }
+}
+
+function bookCover() {
+    indBookCover();
+    if (guessCount < 1 || randomWord === blankArr.join("")) {
+        openBook.style.display = "none";
+        closedBook.style.display = "block";
+    }
+    else {
+        openBook.style.display = "block";
+        closedBook.style.display = "none";
+    }
 }
 
 newGame();
 
 document.onkeyup = function (event) {
-
+    bookCover();
     //holds user input value
     var userGuess = event.key.toLowerCase();
     //checks for a correct key answer
@@ -94,23 +128,13 @@ document.onkeyup = function (event) {
     }
     checkLoss();
     checkWin();
-
-    //test
-
-    //displays array without commas with .join method
-    guessWordText.textContent = blankArr.join("");
-    winsText.textContent = "Wins: " + winCount;
-    lossesText.textContent = "Losses: " + lossCount;
-    startPage.textContent = ""
-    guessString.textContent = guesses;
-    guessCountText.textContent = "You have " + guessCount + " guesses remaining.";
+    keepScore();
 
 };
 
 /* Want
 
-Display book close at win/lose
-Restart by pressing any key
+add specific book covers to each book-close
 add spaces
 
 */
